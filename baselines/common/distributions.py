04c005076aae97ffecf06b11fe6d66f9a02740e9
Max Ferguson <maxkferg@gmail.com>
2017-10-20 16:36:58 -0700

    Fix bugs introduced by merge

--- baselines/common/distributions.py
+++ baselines/common/distributions.py
@@ -229,17 +229,17 @@ class BernoulliPd(Pd):
 
 def make_pdtype(ac_space):
     from gym import spaces
-    #if isinstance(ac_space, spaces.Box):
-     #  assert len(ac_space.shape) == 1
-    return DiagGaussianPdType(ac_space.shape[0])
-    #elif isinstance(ac_space, spaces.Discrete):
-    #    return CategoricalPdType(ac_space.n)
-   # elif isinstance(ac_space, spaces.MultiDiscrete):
-     #   return MultiCategoricalPdType(ac_space.low, ac_space.high)
-  #  elif isinstance(ac_space, spaces.MultiBinary):
- #       return BernoulliPdType(ac_space.n)
-#    else:
-#        raise NotImplementedError
+    if isinstance(ac_space, spaces.Box):
+        assert len(ac_space.shape) == 1
+        return DiagGaussianPdType(ac_space.shape[0])
+    elif isinstance(ac_space, spaces.Discrete):
+        return CategoricalPdType(ac_space.n)
+    elif isinstance(ac_space, spaces.MultiDiscrete):
+        return MultiCategoricalPdType(ac_space.low, ac_space.high)
+    elif isinstance(ac_space, spaces.MultiBinary):
+        return BernoulliPdType(ac_space.n)
+    else:
+        raise NotImplementedError
 
 def shape_el(v, i):
     maybe = v.get_shape()[i]
