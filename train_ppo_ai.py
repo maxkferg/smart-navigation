6a6de39bd55622d7f7274c8408d3d95168bf1418
Max Ferguson <maxkferg@gmail.com>
2017-11-03 17:00:33 -0700

    Larger RNN

--- train_ppo_ai.py
+++ train_ppo_ai.py
@@ -29,7 +29,7 @@ def train(env_id, num_timesteps, seed, evaluate, render):
     eval_env = LearningEnvironment(num_particles=PARTICLES, disable_render=disable_render)
 
     def policy_fn(name, ob_space, ac_space):
-        return RnnPolicy(name=name, ob_space=ob_space, ac_space=ac_space, hid_size=256, rnn_hid_units=128, num_hid_layers=4)
+        return RnnPolicy(name=name, ob_space=ob_space, ac_space=ac_space, hid_size=256, rnn_hid_units=256, num_hid_layers=4)
     #env = bench.Monitor(env, logger.get_dir() and
     #    os.path.join(logger.get_dir(), "monitor.json"))
     #env.seed(seed)
